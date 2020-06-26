1f87e209e36a8a220fe2493765f78e2e83c6dd84
Md. Mahbubur Rahman <semahbub@gmail.com>
2020-06-26 15:05:09 +0600

    Share data on June 26, 2020

---
+++ Ajax_data/DivList.sql
@@ -0,0 +1,15 @@
+USE [AjaxDB]
+SET IDENTITY_INSERT [dbo].[divList] ON
+
+INSERT INTO divList (id, name) VALUES
+(1, 'Barisal'),
+(2, 'Chittagong'),
+(3, 'Dhaka'),
+(4, 'Khulna'),
+(5, 'Rajshahi'),
+(6, 'Rangpur'),
+(7, 'Sylhet'),
+(8, 'Mymensingh');
+
+
+SET IDENTITY_INSERT [dbo].[divList] OFF
